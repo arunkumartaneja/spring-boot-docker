5672b477d885cb5ebabee2d21f53c1886b2a4c1b
sga-arun <arun.taneja@sgainc.com>
2020-07-07 23:15:52 +0530

    initial commit

---
+++ src/main/java/com/docker/demo/controller/DockerController.java
@@ -0,0 +1,13 @@
+package com.docker.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
+}
